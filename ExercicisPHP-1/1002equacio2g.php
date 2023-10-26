d0871bd937e5f60227b4750af06791165fabe2f1
Marc Morlá Isern <marc.morla20@gmail.com>
2023-10-26 09:59:02 +0200

    exercicis 10-12

--- ExercicisPHP-1/1002equacio2g.php
+++ ExercicisPHP-1/1002equacio2g.php
@@ -9,11 +9,22 @@
 
 <body>
     <?php
-        $a;
-        $b;
-        $c;
+        $a = 3;
+        $b = 6;
+        $c = 2;
 
+        $arrel = $b * $b - 4 * $a * $c;
         
+        if ($arrel > 0) {
+            $resultat1 = (-$b + sqrt($arrel)) / (2 * $a);
+            $resultat2 = (-$b - sqrt($arrel)) / (2 * $a);
+            echo "Hi ha dues solucions: $resultat1 i $resultat2";
+        } elseif ($arrel == 0) {
+            $resultat3 = -$b / (2 * $a);
+            echo "Hi ha una solució: $resultat3";
+        } else {
+            echo "No hi ha solució";
+        }
     ?>
 </body>
 
